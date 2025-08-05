494a10fda2d1d2ae0e6bbc992f8dcd0acef40151
Patrick P.C. <pratikchaudhary270@gmail.com>
2025-08-05 23:38:05 +0545

    feat: add responsive footer components

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer>
+    <div class="container">
+            <div class="footer-content">
+               <h3>Contact Us</h3>
+               <p> <i class="fas fa-phone"></i> 000-00000/0000-000000 </p>
+               <p> <i class="fa-solid fa-radio"></i> Everest Engineering College</p>
+               <p> <i class="fas fa-envelope"></i> pratikchaudhary270@gmail.com </p>
+               <p> <i class="fas fa-map-marker-alt"></i> Bardiya, Nepal </p>
+            </div>
+            <div class="footer-content">
+                <h3>Quick Links</h3>
+                 <ul class="list">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#affilates">Plans</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>                  
+                    <li><a href="login.php">login</a></li>
+                 </ul>
+            </div>
+            <div class="footer-content">
+                <h3>Follow Us</h3>
+                <ul class="social-icons">
+                 <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                 <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                 <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                 <li><a href=""><i class="fab fa-linkedin"></i></a></li>
+                </ul>
+                </div>
+        </div>
+        <div class="bottom-bar">
+            <p>&copy; © 2081| All Rights Reserved.</p>
+    </div>
+</footer>
