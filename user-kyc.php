c8bb5a8f14b57951af8301bb918332e641a5e7af
Patrick P.C. <pratikchaudhary270@gmail.com>
2025-08-10 23:07:50 +0545

    feat: implement user KYC form with front/back citizenship upload

---
+++ user-kyc.php
@@ -0,0 +1,213 @@
+
+<?php
+include 'db_conn.php'; // Assuming this file contains the PDO connection
+
+if (isset($_POST['submit'])) {
+    // Use prepared statements to avoid SQL injection
+    $fullname = $_POST['fullname'];
+    $dob = $_POST['dob'];
+    $fname = $_POST['fname'];
+ 
+
+    // File upload handling
+    $citizenf = $_FILES['citizenf'];
+    $citizenb = $_FILES['citizenb'];
+
+    // Define the upload directory
+    $uploadDir = 'uploads/citizenship/';
+
+    // Handle citizenship front photo upload
+    $citizenfName = basename($citizenf['name']);
+    $citizenfTarget = $uploadDir . uniqid() . '_' . $citizenfName;
+    if (move_uploaded_file($citizenf['tmp_name'], $citizenfTarget)) {
+        echo "Front photo uploaded successfully.<br>";
+    } else {
+        echo "Error uploading front photo.<br>";
+    }
+
+    // Handle citizenship back photo upload
+    $citizenbName = basename($citizenb['name']);
+    $citizenbTarget = $uploadDir . uniqid() . '_' . $citizenbName;
+    if (move_uploaded_file($citizenb['tmp_name'], $citizenbTarget)) {
+        echo "Back photo uploaded successfully.<br>";
+    } else {
+        echo "Error uploading back photo.<br>";
+    }
+
+    // Insert data using a prepared statement
+    $insert = "INSERT INTO kyc (fullname, dob, fname, citizenf_path, citizenb_path)
+               VALUES (:fullname, :dob, :fname,  :citizenf_path, :citizenb_path)";
+    
+    $stmt = $conn->prepare($insert);
+    $stmt->bindParam(':fullname', $fullname);
+    $stmt->bindParam(':dob', $dob);
+    $stmt->bindParam(':fname', $fname);
+    $stmt->bindParam(':citizenf_path', $citizenfTarget);
+    $stmt->bindParam(':citizenb_path', $citizenbTarget);
+
+    if ($stmt->execute()) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error inserting record";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ADMIN</title>
+
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- bootstrap 5 CDN-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <!-- bootstrap 5 Js bundle CDN-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" href="assets/css/style.scss">
+</head>
+<body>
+	<div class="container">
+		<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <div class="container-fluid">
+		    <a class="navbar-brand" href="admin.php">Admin</a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse" 
+		         id="navbarSupportedContent">
+		      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+		        <li class="nav-item">
+		          <a class="nav-link" 
+		             aria-current="page" 
+		             href="index.php">Store</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" 
+		             href="add-book.php">Add Book</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" 
+		             href="add-category.php">Add Category</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" 
+		             href="add-author.php">Add Author</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" 
+		             href="logout.php">Logout</a>
+		        </li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav> -->
+
+		<body id="body-pd">
+        <header class="header" id="header">
+            <div class="header__toggle">
+                <i class='bx bx-menu' id="header-toggle"></i>
+            </div>
+
+            <div class="header__img">
+                <img src="assets/img/perfil.jpg" alt="">
+            </div>
+        </header>
+
+        <div class="l-navbar" id="nav-bar">
+            <nav class="nav">
+                <div>
+                    <a href="#" class="nav__logo">
+						
+                        <i class='bx bx-layer nav__logo-icon'></i>
+                        <span class="nav__logo-name">User Dashboard</span>
+                    </a>
+
+                    <div class="nav__list">
+                        <a href="user-dashbord.php" class="nav__link active">
+                        <i class='bx bxs-user'></i>
+                            <span class="nav__name">My Profile</span>
+                        </a>
+
+                        <a href="user-plan.php" class="nav__link">
+						<i class='bx bx-cart-add' ></i>
+                            <span class="nav__name">New SIPs</span>
+                        </a>
+                        
+                        <a href="#" class="nav__link">
+							<i class='bx bx-clipboard'></i>
+                            <span class="nav__name">Update KYC </span>
+                        </a>
+                
+                        <a href="#" class="nav__link">
+                            <i class='bx bx-folder nav__icon' ></i>
+                            <span class="nav__name">MY SIPs</span>
+                        </a>
+
+                        <a href="#" class="nav__link">
+                            <i class='bx bx-bar-chart-alt-2 nav__icon' ></i>
+                            <span class="nav__name">Analytics</span>
+                        </a>
+
+						<a href="index.php" class="nav__link">
+						<i class='bx bx-home'></i>
+                            <span class="nav__name">Home</span>
+                        </a>
+                    </div>
+                </div>
+
+                <a href="user-logout.php" class="nav__link">
+                    <i class='bx bx-log-out nav__icon' ></i>
+                    <span class="nav__name">Log Out</span>
+                </a>
+            </nav>
+        </div>
+
+
+        <form class="KYC" action="" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Name*</label>
+                <input type="text" name="fullname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                
+            </div>
+            
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Date of birth*</label>
+                <input type="text" name="dob" class="form-control" id="exampleInputPassword1">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Fathers Name*</label>
+                <input type="text" name="fname" class="form-control" id="exampleInputPassword1">
+            </div>
+
+            
+
+            <div class="mb-3">
+                <label for="formFile" class="form-label">Citizenship card Front*</label>
+                <input class="form-control" name="citizenf" type="file" id="formFile">
+            </div>
+
+            <div class="mb-3">
+                <label for="formFile" class="form-label">Citizenship card Back*</label>
+                <input class="form-control" name="citizenb" type="file" id="formFile">
+            </div>
+            <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+        </form>
+        
+
+      
+
+
+
+	
+       <script src="assets/js/main.js"></script>
+
+</body>
+</html>
+
